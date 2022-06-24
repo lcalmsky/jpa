f0914d403c6fd064b69983e7889cc716cd4f3886
Jaime <lcalmsky@gmail.com>
2022-06-25 01:16:40 +0900

    Initial commit

---
+++ src/test/java/com/tistory/jaimenote/jpa/JpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tistory.jaimenote.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
