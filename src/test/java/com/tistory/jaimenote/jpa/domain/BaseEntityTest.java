942982eabd45d1c583b923fe8ce6a862f692ee80
Jaime <lcalmsky@gmail.com>
2022-07-11 22:02:20 +0900

    데이터 타입 작성 완료: #24

--- src/test/java/com/tistory/jaimenote/jpa/domain/BaseEntityTest.java
+++ src/test/java/com/tistory/jaimenote/jpa/domain/BaseEntityTest.java
@@ -1,30 +1,30 @@
-package com.tistory.jaimenote.jpa.domain;
-
-import com.tistory.jaimenote.jpa.inheritance.entity.Product;
-import com.tistory.jaimenote.jpa.inheritance.infra.repository.ProductRepository;
-import com.tistory.jaimenote.jpa.relation.domain.entity.Member;
-import com.tistory.jaimenote.jpa.relation.infra.repository.MemberRepository;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.annotation.Rollback;
-
-@DataJpaTest
-class BaseEntityTest {
-
-  @Autowired
-  MemberRepository memberRepository;
-
-  @Autowired
-  ProductRepository productRepository;
-
-  @Test
-  @Rollback(false)
-  void baseEntityTest() {
-    Member member = Member.create("name", "city", "street", "zipcode", null);
-    memberRepository.save(member);
-
-    Product product = Product.create("name", 1000);
-    productRepository.save(product);
-  }
-}
+//package com.tistory.jaimenote.jpa.domain;
+//
+//import com.tistory.jaimenote.jpa.inheritance.entity.Product;
+//import com.tistory.jaimenote.jpa.inheritance.infra.repository.ProductRepository;
+//import com.tistory.jaimenote.jpa.relation.domain.entity.Member;
+//import com.tistory.jaimenote.jpa.relation.infra.repository.MemberRepository;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+//import org.springframework.test.annotation.Rollback;
+//
+//@DataJpaTest
+//class BaseEntityTest {
+//
+//  @Autowired
+//  MemberRepository memberRepository;
+//
+//  @Autowired
+//  ProductRepository productRepository;
+//
+//  @Test
+//  @Rollback(false)
+//  void baseEntityTest() {
+//    Member member = Member.create("name", "city", "street", "zipcode", null);
+//    memberRepository.save(member);
+//
+//    Product product = Product.create("name", 1000);
+//    productRepository.save(product);
+//  }
+//}
